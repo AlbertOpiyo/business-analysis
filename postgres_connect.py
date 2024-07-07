355918991010e045c89204e58ac5a2d22043db84
AlbertOpiyo <110673311+AlbertOpiyo@users.noreply.github.com>
2024-07-07 12:28:34 +0300

    Add files via upload

---
+++ postgres_connect.py
@@ -0,0 +1,27 @@
+import psycopg2
+import streamlit as st
+
+conn = psycopg2.connect(
+        dbname="businessanalysis",
+        user="postgres",
+        password="@Nairobi2003",
+        host="localhost",
+        port=5432
+    )
+
+c = conn.cursor()
+
+def view_all_data():
+	c.execute('SELECT * FROM customers order by id asc')
+	data = c.fetchall()
+	return data
+
+
+
+
+# def view_all_departments():
+# 	c.execute('SELECT Department FROM customers')
+# 	data = c.fetchmany
+# 	return data
+
+
